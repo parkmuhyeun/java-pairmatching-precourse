9147101025a9aa7af7a3ba52c2c94317f1471514
parkmuhyeun <pjhg410@gmail.com>
2022-12-15 13:02:50 +0900

    feat: 기능 선택 입력

---
+++ src/main/java/message/NoticeMessage.java
@@ -0,0 +1,11 @@
+package message;
+
+public class NoticeMessage {
+    public static final String INPUT_SELECT =
+            "기능을 선택하세요.\n" +
+                    "1. 페어 매칭\n" +
+                    "2. 페어 조회\n" +
+                    "3. 페어 초기화\n" +
+                    "Q. 종료";
+
+}
